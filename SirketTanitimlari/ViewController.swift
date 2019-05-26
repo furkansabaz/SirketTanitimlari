3feef55e30d31c6ad28dd97739499c07b97e3f33
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-05-26 12:52:04 +0300

    Initial Commit

---
+++ SirketTanitimlari/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SirketTanitimlari
+//
+//  Created by Furkan Sabaz on 26.05.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
