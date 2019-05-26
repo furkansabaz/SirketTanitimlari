34dbb1aad0d801040565ed3e3f008feffe919772
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-05-26 13:36:06 +0300

    2.Ders Görüntülerin Konumlandırılması

--- SirketTanitimlari/ViewController.swift
+++ SirketTanitimlari/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let googleImage : UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "Google"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        view.addSubview(googleImage)
+        
+        layoutDuzenle()
+        
+        
+        
     }
 
+    
+    private func layoutDuzenle() {
+        googleImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        googleImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        googleImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        
+        
+        googleImage.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        googleImage.heightAnchor.constraint(equalToConstant: 180).isActive = true
+    }
 
 }
 
