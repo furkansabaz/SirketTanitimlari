0cea9bcdd6b8960edd4c74cfa880cc656103c506
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-03 22:02:16 +0300

    Ders 20-Uygulamaya Yatay Mod Desteği

--- SirketTanitimlari/GecisController.swift
+++ SirketTanitimlari/GecisController.swift
@@ -11,7 +11,16 @@ import UIKit
 
 class GecisController : UICollectionViewController , UICollectionViewDelegateFlowLayout{
     
-    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        coordinator.animate(alongsideTransition: { (_) in
+            self.collectionViewLayout.invalidateLayout()
+            let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+            self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }) { (_) in
+            
+        }
+    }
 
     
     let sayfalar =  [
